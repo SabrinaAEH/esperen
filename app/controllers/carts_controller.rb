ca783ccae04f0de675469f9b89e5682347494651
SabrinaAEH <sabrina.aeh@gmail.com>
2024-05-28 15:12:18 +0200

    delete item from cart ok, update qties & total amount in progress

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,24 +3,24 @@ class CartsController < ApplicationController
 
   def show
     @cart = current_user.cart
-
-    if @cart && @cart.cart_items.any?
-      @cart_items = @cart.cart_items.includes(:item)
-    else
-      @cart_items = []
-    end
   end
 
   def update_quantity
-    @cart_item = CartItem.find(params[:id])
+    @cart_item = current_user.cart.cart_items.find(params[:id])
     @cart_item.update(quantity: params[:quantity])
-    redirect_to cart_path, notice: 'Quantité mise à jour avec succès'
+    respond_to do |format|
+      format.html { redirect_to cart_path, notice: 'Quantité mise à jour.' }
+      format.js
+    end
   end
 
   def destroy_item
-    @cart_item = CartItem.find(params[:id])
+    @cart_item = current_user.cart.cart_items.find(params[:id])
     @cart_item.destroy
-    redirect_to cart_path, notice: 'Article supprimé avec succès du panier'
+    respond_to do |format|
+      format.html { redirect_to cart_path, notice: 'Article supprimé du panier.' }
+      format.js
+    end
   end
 
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,14 @@ class ItemsController < ApplicationController
   def add_to_cart
     @item = Item.find(params[:id])
     cart = current_user.cart || current_user.create_cart
-    cart.cart_items.create(item: @item)
+    cart_item = cart.cart_items.find_or_initialize_by(item: @item)
+    cart_item.quantity ||= 0 # Initialisez à 0 si nil
+    cart_item.quantity += 1
+    cart_item.save
 
     respond_to do |format|
       format.html { redirect_to cart_path, notice: 'Item ajouté au panier.' }
-      format.js   # add_to_cart.js.erb
+      format.js
     end
   end
 end
